aebcb0a21b021b318b7f6eeb5bc2c68601243811
Elminstêr <73755282+Elminstr@users.noreply.github.com>
2021-05-21 21:58:50 +0300

    Update davetlerim.js

--- komutlar/davetlerim.js
+++ komutlar/davetlerim.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-    const   db = require("old-wio.db")
+    const   db = require("quick.db")
      
 
 exports.run = async (client, message, args, tools) => {
