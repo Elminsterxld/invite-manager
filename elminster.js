c3b4db33f72d0b1dc948216e7caf51accb4df676
Elminstêr <73755282+Elminstr@users.noreply.github.com>
2021-05-21 22:00:06 +0300

    Create elminster.js

---
+++ elminster.js
@@ -0,0 +1,155 @@
+const invites = {};
+
+const wait = require("util").promisify(setTimeout);
+
+client.on("ready", () => {
+  wait(1000);
+
+  client.guilds.cache.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+});
+client.on("guildMemberRemove", async member => {
+  let kanal = await db.fetch(`davetkanal_${member.guild.id}`);
+  if (!kanal) return;
+  let veri = await db.fetch(`rol1_${member.guild.id}`);
+  let veri12 = await db.fetch(`roldavet1_${member.guild.id}`);
+  let veri21 = await db.fetch(`roldavet2_${member.guild.id}`);
+  let msj = await db.fetch(`davetmsjbb_${member.guild.id}`);
+  let veri2 = await db.fetch(`rol2_${member.guild.id}`);
+  let d = await db.fetch(`bunudavet_${member.id}`);
+  const sa = client.users.cache.get(d);
+  const sasad = member.guild.members.cache.get(d);
+  let sayı2 = await db.fetch(`davet_${d}_${member.guild.id}`);
+  db.add(`davet_${d}_${member.guild.id}`, -1);
+
+  if (!d) {
+    const aa = new Discord.MessageEmbed()
+    .setColor("#E70000")
+      .setDescription(
+        `\`\`${member.user.tag}\`\` **adlı şahıs aramızdan ayrıldı.\nŞahsı davet eden:** \`\`Bulunamadı!\`\``
+      )
+      .setFooter(client.user.username, client.user.avatarURL());
+    client.channels.cache.get(kanal).send(aa);
+    return;
+  } else {
+     if (!msj) {
+    const aa = new Discord.MessageEmbed()
+      .setColor("#E70000")
+      .setDescription(
+        `\`\`${member.user.tag}\`\` **adlı şahıs aramızdan ayrıldı.\nŞahsı davet eden:** \`\`${sa.tag}\`\``
+      )
+      .setFooter(client.user.username, client.user.avatarURL());
+    client.channels.cache.get(kanal).send(aa);
+     return;
+  } else {
+    var msj2 = msj
+      .replace(`-sunucu-`, `${member.guild.name}`)
+      .replace(`-uye-`, `${member.user.tag}`)
+      .replace(`-uyetag-`, `<@${member.user.id}>`)
+      .replace(`-daveteden-`, `${sa.tag}`)
+      .replace(`-davetoldu-`, `${sayı2}`);
+    const embed = new Discord.MessageEmbed()
+   .setColor("#E70000")
+      .setDescription(msj2)
+      .setFooter(client.user.username, client.user.avatarURL());
+    client.channels.cache.get(kanal).send(embed);
+    return;
+  }
+    if (!veri) return;
+
+    if (sasad.roles.cache.has(veri)) {
+      if (sayı2 <= veri12) {
+        sasad.roles.remove(veri);
+        return;
+      }
+    }
+    if (sasad.roles.cache.has(veri2)) {
+      if (!veri2) return;
+      if (sayı2 <= veri21) {
+        sasad.roles.remove(veri2);
+        return;
+      }
+    }
+  }
+});
+
+client.on("guildMemberAdd", async member => {
+  member.guild.fetchInvites().then(async guildInvites => {
+    let veri = await db.fetch(`rol1_${member.guild.id}`);
+    let veri12 = await db.fetch(`roldavet1_${member.guild.id}`);
+    let veri21 = await db.fetch(`roldavet2_${member.guild.id}`);
+    let veri2 = await db.fetch(`rol2_${member.guild.id}`);
+    let kanal = await db.fetch(`davetkanal_${member.guild.id}`);
+    let msj = await db.fetch(`davetmsjhg_${member.guild.id}`);
+    if (!kanal) return;
+    const ei = invites[member.guild.id];
+
+    invites[member.guild.id] = guildInvites;
+
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const sasad = member.guild.members.cache.get(invite.inviter.id);
+    const davetçi = client.users.cache.get(invite.inviter.id);
+
+    db.add(`davet_${invite.inviter.id}_${member.guild.id}`, +1);
+    db.set(`bunudavet_${member.id}`, invite.inviter.id);
+    let sayı = await db.fetch(`davet_${invite.inviter.id}_${member.guild.id}`);
+
+    let sayı2;
+    if (!sayı) {
+      sayı2 = 0;
+    } else {
+      sayı2 = await db.fetch(`davet_${invite.inviter.id}_${member.guild.id}`);
+    }
+    if (!msj) {
+    const aa = new Discord.MessageEmbed()
+       .setColor("#E70000")
+      .setDescription(
+        `\`\`${member.user.tag}\`\` **adlı şahıs sunucuya katıldı.\nŞahsı davet eden:** \`\`${davetçi.tag}\`\`\n**Toplam \`\`${sayı2}\`\` daveti oldu!**`
+      )
+      .setFooter(client.user.username, client.user.avatarURL());
+    client.channels.cache.get(kanal).send(aa);
+    return;
+  } else {
+    var msj2 = msj
+      .replace(`-sunucu-`, `${member.guild.name}`)
+      .replace(`-uye-`, `${member.user.tag}`)
+      .replace(`-uyetag-`, `<@${member.user.id}>`)
+      .replace(`-daveteden-`, `${davetçi.tag}`)
+      .replace(`-davetoldu-`, `${sayı2}`);
+    const embed = new Discord.MessageEmbed()
+       .setColor("#E70000")
+      .setDescription(msj2)
+      .setFooter(client.user.username, client.user.avatarURL());
+    client.channels.cache.get(kanal).send(embed);
+    return;
+  }
+    if (!veri) return;
+
+    if (!sasad.roles.cache.has(veri)) {
+      if (sayı2 => veri12) {
+        sasad.roles.add(veri);
+        return;
+      }
+    } else {
+      if (!veri2) return;
+      if (sayı2 => veri21) {
+        sasad.roles.add(veri2);
+        return;
+      }
+    }
+  });
+});
+
+
+         
+              
+  
+              
+              
+              
+  
+              
+ 
